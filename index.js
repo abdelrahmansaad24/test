38d2cde35da5d8db2bdb73927429c16ec183d559
Abdelrahman Ibrahim Abdelhalim Saad <76453265+abdelrahmansaad24@users.noreply.github.com>
2024-03-12 18:53:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const express = require('express');
-const { spawn } = require('child_process');
 const { py, python } = require('pythonia')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 let np = null ;
-function callPythonScript(callback) {
-  const pythonProcess = spawn('python', ['./test.py']);  // Assuming the Python script is named test.py
-
-  let dataString = '';
-
-  pythonProcess.stdout.on('data', (data) => {
-    dataString += data.toString();
-  });
-
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  pythonProcess.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-    callback(dataString);
-  });
-}
 
 app.get('/', async (req, res) => {
   console.log(await np.plus())
